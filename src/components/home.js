eb2500f39ff33f67b5c934d5ec32a607be551035
Jordan Salisbury <salisburyjordan21@gmail.com>
2019-01-14 12:26:07 -0800

    Project setup in progress

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">Lizard Boih</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo cumque minus Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores quibusdam minima nisi.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis temporibus nulla ab. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid saepe doloribus dolores!</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus quo eveniet labore. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda reprehenderit debitis consequatur.</p>
+    </div>
+)
