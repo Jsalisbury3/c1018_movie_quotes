8f6e761b842032a47dd50fee48e2eef80719267b
Jordan Salisbury <salisburyjordan21@gmail.com>
2019-01-14 17:00:15 -0800

    Project setup done with all routes and navs working

---
+++ src/components/movie_quote.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import axios from 'axios'; 
+
+
+class MovieQuote extends Component {
+    render(){
+        return (
+            <div className="center">
+                <h1 className="center">Movie Quote</h1>
+                <h5>Movie quote: Die lizard boih</h5>
+            </div>
+        );
+    }
+}
+
+
+export default MovieQuote;
